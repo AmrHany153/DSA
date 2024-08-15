38b3fb621d8372fd3f940f9b8b6b9d69ff599401
amrhany153 <amrking36311@gmail.com>
2024-08-15 13:21:04 +0300

    Upload the project

---
+++ sameFrequency/sameFrequency.js
@@ -0,0 +1,14 @@
+// Frequency Counter - sameFrequency
+
+// Write a function called sameFrequency. Given two positive intergers,
+// find out if the two numbers have the same frequency of digits.
+
+// Solution Requirements
+//      - Time Complexity: O(n)
+
+function sameFrequency() {
+
+
+}
+
+export {sameFrequency}
---
+++ sameFrequency/sameFrequency.test.js
@@ -0,0 +1,23 @@
+import {describe, it, expect} from "vitest"
+import {sameFrequency} from "./sameFrequency";
+
+
+describe("sameFrequency", () => {
+    it('1- should returns true', () => {
+        expect(sameFrequency(182, 281)).toBe(true)
+    });
+    it('2- should returns false', () => {
+        expect(sameFrequency(34, 14)).toBe(false)
+    });
+    it('3- should returns true', () => {
+        expect(sameFrequency(3589578, 5879385)).toBe(true)
+    });
+    it('4- should returns true', () => {
+        expect(sameFrequency(22, 222)).toBe(true)
+    });
+})
+
+//  console.log(sameFrequency(182, 281)) // true
+//  console.log(sameFrequency(34, 14)) // false
+//  console.log(sameFrequency(3589578, 5879385)) // true
+//  console.log(sameFrequency(22, 222)) // true
